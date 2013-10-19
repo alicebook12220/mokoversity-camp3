24aeb8eae24019960a0b7018979f1e83a841da99
alicebook12220 <alicebook12220@gmail.com>
2013-10-19 16:23:05 +0800

    fix #7

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-(function(){
-  var canvas=document.getElementById("game");
+var gameModule = (function(){
+  var timeoutVar,
+      counter = 0;
+
+  function start(){
+    var canvas=document.getElementById("game");
   var ctx=canvas.getContext("2d");
 
   var ballX=Math.floor(Math.random()*300); 
@@ -13,4 +17,18 @@
   ctx.beginPath();
   ctx.arc(ballX,ballY,ballR,0,2*Math.PI,true);
   ctx.fill();
+
+  if(counter >= 10){
+
+  }else{
+    timeoutVar = setTimeout(start,1000);
+    counter = counter + 1;
+
+    console.log("Counter: "+counter)
+  }
+  }
+  return{
+    start:start
+  }
 })();
+gameModule.start();
