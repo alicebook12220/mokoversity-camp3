24762ddab6441153f97b078601732a1f0b2678aa
alicebook12220 <alicebook12220@gmail.com>
2013-10-20 11:12:52 +0800

    fix #9

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 var gameModule = (function(){
   var timeoutVar,
-      counter = 0;
-      
+      counter = 0,
+      ballX,
+      ballY,
+      ballR,
+
+      colors = ['#ff0000', '#0000ff', 'yellow'],
+      length = colors.length;
+
   function touchEvent(evt){
     var x=evt.clientX,
         y=evt.clientY;
+        tmp=(ballX-x)*(ballX-x)+(ballY-y)*(ballY-y);
+
     console.log("Clicked: "+x+" , "+y);
+
+    if(tmp<ballR*ballR){
+
+      console.log("Hit! your scores: ");
+    }
   }
 
   function start(){
+
     document.getElementById("main").addEventListener("click", touchEvent,false);
     startGame();
   }
@@ -17,26 +31,26 @@ var gameModule = (function(){
     var canvas=document.getElementById("game");
   var ctx=canvas.getContext("2d");
 
-  var ballX=Math.floor(Math.random()*300); 
-  var ballY=Math.floor(Math.random()*500);
-  var ballR=Math.floor(Math.random()*100);
+      ballX=Math.floor(Math.random()*600); 
+      ballY=Math.floor(Math.random()*450);
+      ballR=Math.floor(Math.random()*80);
 
-  canvas.width = 480;
-  canvas.height = 320;
+  canvas.width = 640;
+  canvas.height = 480;
 
-  ctx.fillStyle = 'black';
+  ctx.fillStyle =colors[counter % length];
   ctx.beginPath();
   ctx.arc(ballX,ballY,ballR,0,2*Math.PI,true);
   ctx.fill();
 
     if(counter >= 10){
-
+      GameOver();
     }else{
-      timeoutVar = setTimeout(start,1000);
+      timeoutVar = setTimeout(startGame,1000);
       counter = counter + 1;
     }
     function GameOver(){
-      console.log("Counter: "+counter);
+      console.log("Final: "+scores);
     }
   }
   return{
